5dc5d1f05949d0bf27000b7687e8b383405be53e
bbsachinbb <bbsachinbb@gmail.com>
2023-02-05 22:14:00 +0530

    Update SearchCard.js

--- src/layout/SearchCard.js
+++ src/layout/SearchCard.js
@@ -1,9 +1,9 @@
 import React, { Fragment, useState } from "react";
 
-export const SearchCard = ({ item }) => {
+export const SearchCard = ({ item, className, index, active }) => {
   const { id, name, address } = item;
   const [isHovering, setIsHovering] = useState(false);
-
+  console.log(index);
   const handleMouseOver = () => {
     setIsHovering(true);
   };
@@ -18,8 +18,13 @@ export const SearchCard = ({ item }) => {
         height: 50,
         border: "1px solid black",
         flexDirection: "column",
-        backgroundColor: isHovering ? "yellow" : "",
+        backgroundColor: isHovering
+          ? "yellow"
+          : index === active
+          ? "yellow"
+          : "",
       }}
+      className={className}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}>
       <Fragment>{id}</Fragment>
